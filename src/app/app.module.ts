b85859510b9484cce5b388759e4b4827cdb3c831
Napisoot <napisoot@foobarsystem.com>
2018-07-21 14:46:33 +0700

    add theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { FeatureComponent } from './pages/feature/feature.component';
+import { JumpbotronComponent } from './pages/jumpbotron/jumpbotron.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactComponent
+    ContactComponent,
+    HeaderComponent,
+    FeatureComponent,
+    JumpbotronComponent
   ],
   imports: [
     BrowserModule,
